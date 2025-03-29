b665486e261aeba434e824ef220e7e3fb995a9e3
dustech <stefano.cerruti@outlook.com>
2025-03-29 11:38:50 +0100

    Add documentation files

---
+++ README.md
@@ -0,0 +1,13 @@
+## Environments
+
+
+#### Configuration ( both UAT and PROD )
+
+Env File /etc/quicksilvra/quicksilvra-env.sh
+
+#### Deploy ( both UAT and PROD )
+
+Files in /opt/deploy
+
+
+
---
+++ uat/quicksilvra=env.sh
@@ -0,0 +1,15 @@
+# hosting
+
+export Quicksilvra_DataProtectionConfiguration__X509__FileName="filename"
+export Quicksilvra_DataProtectionConfiguration__X509__Key="filename password"
+export Quicksilvra_DataProtectionConfiguration__X509__Path="/etc/quicksilvra/data-protection"
+export Quicksilvra_DataProtectionConfiguration__DataProtectionPath="/etc/quicksilvra/data-protection/keys"
+
+
+export Quicksilvra_Uat_AuthorityInternalUri="http://idp:5001"
+export Quicksilvra_Uat_AuthorityExternalUri="https://uat.idp.quicksilvra.com"
+
+
+export DuendeIdentityServerLicense=""
+
+
